e6977b3daf494e48a1765ffc0c1f2fb5e321951f
Mike Owens <mocoding86@gmail.com>
2020-11-23 17:17:30 -0800

    add dynamic functionality with adding and removing ingredients and addition and reduction handlers for the total price

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -16,8 +16,8 @@ const burger = (props) => {
 
     let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        return [...Array(props.ingredients[igKey])].map(( _, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
         });
     })
     .reduce((arr, el) => {
