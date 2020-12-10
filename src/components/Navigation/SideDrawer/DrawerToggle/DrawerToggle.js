2e228203bfb43721df2324e876dcc819dcd00824
Mike Owens <mocoding86@gmail.com>
2020-12-09 17:47:34 -0800

    add hamburger bar on mobile size

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const drawerToggle = (props) => {
-    
-    return (
-        <div onClick={props.clicked}>MENU</div>
-    )
+import classes from './DrawerToggle.css';
+
+const drawerToggle = (props) => (
     
 
-};
+        <div className={classes.DrawerToggle} onClick={props.clicked}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+);
 
 export default drawerToggle;
