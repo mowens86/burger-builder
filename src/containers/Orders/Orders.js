680258b561c4395cb45f97db54e7d7b463c676f1
Mike Owens <mocoding86@gmail.com>
2021-01-04 19:08:28 -0800

    implement navigation links

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Order from '../../components/Order/Order';
 
-class Order extends Component {
+class Orders extends Component {
 
     render () {
 
@@ -15,4 +15,4 @@ class Order extends Component {
     }
 }
 
-export default Order;
+export default Orders;
