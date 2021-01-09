bd025ef5d52e1861a4b6222dbcd89f13ba8bca7a
Mike Owens <mocoding86@gmail.com>
2021-01-08 16:14:27 -0800

    add error message to forms

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -46,11 +46,17 @@ const input = (props) => {
                 value={props.value}
                 onChange={props.changed}/>;
     }
+
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid {props.valueType}!</p>;
+    }
     
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 
