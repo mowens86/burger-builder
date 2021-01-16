ba5cff95941a06f10436163cffc7a65ded3a1dbf
Mike Owens <mocoding86@gmail.com>
2021-01-16 15:05:56 -0800

    worked order actions and updated redirect

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -17,19 +17,22 @@ class Checkout extends Component {
 
 
     render () {
+        let summary = <Redirect to="/" />;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                        ingredients={this.props.ings}
+                        onCheckoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}/>
+                    <Route 
+                        path={`${this.props.match.path}/contact-data`} 
+                        component={ContactData} />
+                </div>
+            );
+        };
         // console.log(this.props);
-        return (
-            <div>
-                <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    onCheckoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route 
-                    path={`${this.props.match.path}/contact-data`} 
-                    component={ContactData} />
-            </div>
-        )
+        return summary;
     }
 
 }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
     state = {
@@ -95,33 +97,23 @@ class ContactData extends Component {
                 valid: true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
-        this.setState({ loading: true });
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm) {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
-        const orders = {
+        const order = {
             ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
             
         }
 
-        // For firebase need to add .json for data to be input and posted
-        axios.post('/orders.json', orders)
-            .then(response => {
-                this.setState({ loading: false });
-                this.props.history.push('/');
-            })
-            .catch(error => {
-                this.setState({ loading: false });
-            }); 
+        this.props.onOrderBurger(order);
 
     }
 
@@ -191,7 +183,7 @@ class ContactData extends Component {
                 <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />;
         }
         return (
@@ -207,8 +199,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
-    }
-}
+        price: state.totalPrice,
+        loading: state.loading
+    };
+};
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
