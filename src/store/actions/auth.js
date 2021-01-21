1bb4846647a52ed6b518cc249b69fc60a659763b
Mike Owens <mocoding86@gmail.com>
2021-01-20 17:12:54 -0800

    debug failed signin, found if statement was not syntactically correct

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,22 +23,24 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
-        }
-        axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, authData)
+        };
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
+        if (!isSignup) url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
+        axios.post(url, authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data))
+            dispatch(authSuccess(response.data));
         })
         .catch(err => {
             console.log(err);
             dispatch(authFail(err));
-        })
+        });
     };
 };
