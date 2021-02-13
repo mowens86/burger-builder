e11c51fc5f9652ee7221983ccdfc0866dc97a3fd
Mike Owens <mocoding86@gmail.com>
2021-02-13 12:04:07 -0800

    convert modal to functional component with hooks and use react memo to optimize

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-class Modal extends Component {
+const modal = props => {
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    // }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-
-    componentDidUpdate() {
-        // console.log('[Modal] Will update');
-    }
-
-    render () {
         return (
             <Auxiliary>
             <Backdrop 
-                show={this.props.show} 
-                clicked={this.props.modalClosed} />
+                show={props.show} 
+                clicked={props.modalClosed} />
                     <div 
                     className={classes.Modal}
                     style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
+                        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: props.show ? '1' : '0'
                     }}>
-                        {this.props.children}
+                        {props.children}
                     </div>
             </Auxiliary>
         )
-    }
-}
-export default Modal;
+};
+
+export default React.memo(
+    modal, 
+    (prevProps, nextProps) =>
+        nextProps.show === prevProps.show && 
+        nextProps.children === prevProps.children
+    );
